2721d03a6247719654c196d7d04e2ce00d0709a0
Rik Helsen <rik_helsen@hotmail.com>
2021-07-06 22:32:02 +0200

    :recycle: isorted imports

--- user_data/strategies/MoniGoManiHyperStrategy.py
+++ user_data/strategies/MoniGoManiHyperStrategy.py
@@ -1,7 +1,9 @@
 # pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
 # flake8: noqa: F401
-# isort: skip_file
 # --- ↑↓ Do not remove these libs ↑↓ -----------------------------------------------------------------------------------
+import sys
+from pathlib import Path
+
 import numpy as np  # noqa
 import pandas as pd  # noqa
 import talib.abstract as ta
@@ -9,11 +11,8 @@ from pandas import DataFrame
 
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 
-# Strategy specific imports, files must reside in same folder as strategy
-import sys
-from pathlib import Path
+# Master Framework file must reside in same folder as Strategy file
 sys.path.append(str(Path(__file__).parent))
-
 from MasterMoniGoManiHyperStrategy import MasterMoniGoManiHyperStrategy
 # ---- ↑ Do not remove these libs ↑ ------------------------------------------------------------------------------------
 
