cc64469bb8e4c2d6d5774f8d8e250f13992efcc7
topscoder <topscoder@gmail.com>
2021-07-15 22:02:07 +0200

    Writing first pytest

--- tests/test_mgm_hurry.py
+++ tests/test_mgm_hurry.py
@@ -5,18 +5,7 @@ sys.path.append("..")
 from user_data.mgm_tools.mgm_hurry.freqtrade_cli import FreqtradeCli
 
 def test_initialisation_without_logger():
-    fc = FreqtradeCli('.', None)
-    assert isinstance(fc, FreqtradeCli)
-
-def test_initialisation_with_logger():
-    '''
-    Todo:
-        - Mock logger object
-    '''
-    from user_data.mgm_tools.mgm_hurry.LeetLogger import get_logger
-    logger = get_logger()
-
-    fc = FreqtradeCli('.', logger)
+    fc = __get_instance('.', use_logger = False)
     assert isinstance(fc, FreqtradeCli)
 
 def test_initialisation_with_logger():
@@ -27,8 +16,22 @@ def test_initialisation_with_logger():
         - Mock logger object
         - Probably mock freqtrade installation
     '''
-    from user_data.mgm_tools.mgm_hurry.LeetLogger import get_logger
-    logger = get_logger()
-
-    fc = FreqtradeCli('.', logger)
+    fc = __get_instance('.', use_logger = True)
     assert isinstance(fc, FreqtradeCli)
+
+def test_set_basedir():
+    fc = __get_instance('.', use_logger=True)
+    assert fc.basedir == '.'
+
+'''
+Private helper methods
+'''
+def __get_instance(dir, use_logger = True):
+    logger = None
+
+    if use_logger is True:
+        from user_data.mgm_tools.mgm_hurry.LeetLogger import get_logger
+        logger = get_logger()
+
+    fc = FreqtradeCli(dir, logger)
+    return fc
