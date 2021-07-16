d5082f5bc629135b0cf76066ab326f914816ca0f
topscoder <topscoder@gmail.com>
2021-07-16 09:46:08 +0200

    Added main interface test

---
+++ tests/test_mgm_hurry_interface.py
@@ -0,0 +1,16 @@
+import pytest
+
+'''
+This unit test file will help while splitting mgm-hurry
+into more modular code.
+
+  1. Create unit test for specific functionality
+     - Verify unit test succeeds
+  2. Split and modularize code
+  3. Run unit test
+     - Verify unit test still succeeds
+'''
+
+@pytest.mark.skip(reason="Test not implemented.")
+def test_basic_usage():
+  assert NotImplemented
