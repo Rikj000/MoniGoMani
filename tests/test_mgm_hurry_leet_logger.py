b51804ee460799bdf86af3732d90436905feead8
topscoder <topscoder@gmail.com>
2021-07-16 19:32:41 +0200

    Fixing pylint advices

--- tests/test_mgm_hurry_leet_logger.py
+++ tests/test_mgm_hurry_leet_logger.py
@@ -1,10 +1,10 @@
 from logging import Logger
+from user_data.mgm_tools.mgm_hurry.leet_logger import get_logger
+
 import sys
 sys.path.append(".")
 sys.path.append("..")
 
-from user_data.mgm_tools.mgm_hurry.leet_logger import get_logger
-
 def test_initialisation_logger():
-  logger = get_logger()
-  assert type(logger) is Logger
+    logger = get_logger()
+    assert type(logger) is Logger
