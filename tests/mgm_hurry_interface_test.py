faa83d74d6671e6407d73fc7cb7810cd0edee2f0
topscoder <topscoder@gmail.com>
2021-07-20 13:39:13 +0200

    Fixing action runners

---
+++ tests/mgm_hurry_interface_test.py
@@ -0,0 +1,31 @@
+import pytest
+
+# This unit test file will help while splitting mgm-hurry
+# into more modular code.
+#
+#   1. Create unit test for specific functionality
+#      - Verify unit test succeeds
+#   2. Split and modularize code. The interface should remain unchanged.
+#   3. Run unit test
+#      - Verify unit test still succeeds
+#
+# Also be aware to unit test on code and cli level if differences could occur.
+#
+# Interface mgm-hurry:
+#    MGMHurry.
+#       up
+#       install_freqtrade
+#       install_mgm
+#       setup
+#       cleanup
+#       download_candle_data
+#       hyperopt
+#       hyperopt_show_results
+#       hyperopt_show_epoch
+#       hyperopt_apply_epoch
+#       backtest
+#       start_trader
+
+@pytest.mark.skip(reason='Test not implemented.')
+def test_basic_usage():
+    assert NotImplemented
---
+++ tests/mgm_hurry_monigomani_logger_test.py
@@ -0,0 +1,12 @@
+from logging import Logger
+from user_data.mgm_tools.mgm_hurry.MoniGoManiLogger import get_logger
+
+import sys
+
+sys.path.append('.')
+sys.path.append('..')
+
+
+def test_initialisation_logger():
+    logger = get_logger()
+    assert type(logger) is Logger
