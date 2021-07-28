c5d1e2ca7415cc46af3771d02c513c7eca5269df
topscoder <86197446+topscoder@users.noreply.github.com>
2021-07-28 21:39:07 +0200

    Adding tests

--- tests/mgm_hurry_monigomani_config_test.py
+++ tests/mgm_hurry_monigomani_config_test.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('.')
 sys.path.append('..')
 
+from pytest import mark
 from user_data.mgm_tools.mgm_hurry.MoniGoManiConfig import MoniGoManiConfig
 from user_data.mgm_tools.mgm_hurry.MoniGoManiLogger import get_logger
 
@@ -13,6 +14,16 @@ from user_data.mgm_tools.mgm_hurry.MoniGoManiLogger import get_logger
 def test_initialisation():
     cfg = __get_instance('.', get_logger())
     assert type(cfg) is MoniGoManiConfig
+
+def test_valid_config_file_present_should_return_false_without_hurry_config():
+    obj = __get_instance('.', get_logger())
+    assert obj.valid_config_file_present() is False
+
+
+@mark.notimplemented
+def test_config_getter_method_should_return_dict():
+    # TODO use mock / stub to create .hurry file
+    assert True is True
 
 def __get_instance(basedir='.', logger=None):
     return MoniGoManiConfig(basedir, logger)
