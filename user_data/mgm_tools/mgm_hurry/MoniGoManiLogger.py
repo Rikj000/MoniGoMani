91321fda740ea08f15628ffec700ee23b10ac5ca
topscoder <86197446+topscoder@users.noreply.github.com>
2021-08-16 21:58:57 +0200

    Implement mgmlogger as singleton

--- user_data/mgm_tools/mgm_hurry/MoniGoManiLogger.py
+++ user_data/mgm_tools/mgm_hurry/MoniGoManiLogger.py
@@ -168,7 +168,15 @@ class MGMLogger(logging.Logger):
 
         return line
 
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
 
+@singleton
 class MoniGoManiLogger():
     """
     Let's Log and Roll.
